756a2178e6333f34cc3703768968f3f4e6938346
brianpaden <brian.paden@samsung.com>
2018-10-05 08:57:36 -0700

    [TEST] Added interpolation test template

---
+++ test/test_glc_interpolation.cpp
@@ -0,0 +1,41 @@
+/* Copyright (C) Brian Paden (bapaden@mit.edu) - All Rights Reserved
+ * Written by Brian Paden
+ * Released under the GNU General Public License v3
+ */
+
+#include<glc/glc_interpolation.h>
+#include<gtest/gtest.h>
+
+using namespace glc;
+
+/**
+ * 
+ */
+TEST(GlcInterpolation,Constructor){
+}
+
+/**
+ * 
+ */
+TEST(GlcInterpolation,Concatenation){
+}
+
+/**
+ * 
+ */
+TEST(GlcInterpolation,Push){
+}
+
+/**
+ * 
+ */
+TEST(GlcInterpolation,At){
+}
+
+
+int main(int argc, char** argv){
+  
+  ::testing::InitGoogleTest(&argc,argv);
+  return RUN_ALL_TESTS();
+  
+}
