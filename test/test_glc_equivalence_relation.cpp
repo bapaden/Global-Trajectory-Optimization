8bbad8ddb5959f57b8da7063bf13f5cfd0eaa652
brianpaden <brian.paden@samsung.com>
2018-10-05 08:56:57 -0700

    [TEST] Renamed planner_core test

---
+++ test/test_glc_equivalence_relation.cpp
@@ -0,0 +1,33 @@
+#include<glc_state_equivalence_class.h>
+#include<gtest/gtest.h>
+
+using namespace glc;
+
+/**
+ */
+TEST(GlcEquivalenceRelation,Order){
+  
+  StateEquivalenceClass d0;
+  StateEquivalenceClass d1;
+  StateEquivalenceClass d2;
+  
+  d0.coordinate = std::vector<int>({1,2,3});
+  d1.coordinate = std::vector<int>({1,3,3});
+  d2.coordinate = std::vector<int>({1,2,3});
+  
+  EXPECT_TRUE(d0<d1);
+  EXPECT_FALSE(d1<d0);
+  
+  EXPECT_TRUE(d2<d1);
+  EXPECT_FALSE(d1<d2);
+  
+  EXPECT_FALSE(d0<d2);
+  EXPECT_FALSE(d2<d0);
+}
+
+int main(int argc, char** argv){
+  
+  ::testing::InitGoogleTest(&argc,argv);
+  return RUN_ALL_TESTS();
+  
+}
